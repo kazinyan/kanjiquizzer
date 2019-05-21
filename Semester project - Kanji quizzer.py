bc76a5c5727ea58e1debc8a783779da8f6b39a86
kazinyan <50857105+kazinyan@users.noreply.github.com>
2019-05-20 23:58:37 -0500

    Update Semester project - Kanji quizzer.py

--- Semester project - Kanji quizzer.py
+++ Semester project - Kanji quizzer.py
@@ -4,20 +4,6 @@ import random
 kanji_dict = {}
 learned_list = {}
 
-# WHAT THIS PROGRAM DOES:
-# Reads from a master file of about 2000 Kanji and their readings
-# The file can be found here: http://nihongo.monash.edu/jouyoureadings.html
-# User can choose how many terms they want to learn at at time. This learned list only grows as the user calls the
-# function more and more.
-
-# Japanese has two phonetic alphabets, Hiragana and Katakana. Each have 46 characters.
-# Kanji have either a Onyomi and/or a Kunyomi reading. Onyomi is closer to the original Chinese reading while
-# Kunyomi readings are traditional Japanese readings
-# Onyomi readings are written in Katakana while Kunyomi readings are in Hiragana
-# Romaji is the English characterization of Hiragana/Katakana. Ex: a = あ. It's not always perfect however
-# The jaconv module helps normalize all these different readings
-
-
 def menu():
     print('p to print out kanji_dict\n'
           'c to clear learned list\n'
